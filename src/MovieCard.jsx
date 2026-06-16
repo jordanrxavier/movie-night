37ce8eb8dd8f2df2ab9573f68a66b28d4a773648
Jordan Xavier <jordanrxavier@gmail.com>
2026-06-16 12:34:11 -0400

    part 2 complete

--- src/MovieCard.jsx
+++ src/MovieCard.jsx
@@ -3,9 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 export default function MovieCard({ movie }) {
+    let watchedText;
+    if (movie.watched) {
+        watchedText = "Watched";
+    } else {
+        watchedText = "Not watched yet";
+    }
+
     return (
         <div>
             {movie.title},{movie.genre},{movie.year}
+            <p>{watchedText}</p>
         </div>
     )
 }
